f5c5c4785469fca71b538d03e5793b8f1c3f2f33
sstevenadi <sstevenadi@gmail.com>
2021-05-08 21:06:23 +0700

    Update Option
    Setting Volume dan pemindahan beberapa prefab ke foldernya masing-masing

---
+++ Assets/Scripts/Player/VolumeSetting.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeSetting : MonoBehaviour
+{
+    private float earVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<AudioListener>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        AudioListener.volume = earVolume;
+    }
+
+    public void SetVolume(float vol) {
+        earVolume = vol;
+    }
+}
