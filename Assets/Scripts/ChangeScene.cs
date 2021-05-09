7f766c436e8e5e714f24a447e64e51ff5ff9d8a8
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-05-09 21:49:17 +0700

    Anim Pintu dan Player
    Pintu Buka dan Player Maju di MAIN MENU

--- Assets/Scripts/ChangeScene.cs
+++ Assets/Scripts/ChangeScene.cs
@@ -5,9 +5,24 @@ using UnityEngine.SceneManagement;
 
 public class ChangeScene : MonoBehaviour
 {
+    public Animator animPintu, animPlayer;
+    public GvrEditorEmulator GvrEditorEmulator;
+
+    private IEnumerator IeAnimasi(string scene)
+    {
+        animPintu.Play("Anim Pintu Buka");
+        animPlayer.Play("Anim Masuk Player In");
+        GetComponent<Animator>().Play("RMapAnim");
+        // GvrEditorEmulator.enabled = false;
+        yield return new WaitForSeconds(3.5f);
+        SceneManager.LoadScene(scene);
+        
+    }
+    
     // Untuk menganti scene
-    public void SceneChange(string sceneName) {
-        SceneManager.LoadScene(sceneName);
+    public void SceneChange(string sceneName)
+    {
+        StartCoroutine(IeAnimasi(sceneName));
     }
 
     // Keluar dari game
