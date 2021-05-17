d7fa62a9779536c17e7eee46c46d730d452aabf7
sstevenadi <sstevenadi@gmail.com>
2021-05-17 09:01:16 +0700

    nambah spinn dan beberapa asset di hutan

--- Assets/Scripts/Map/Spinning.cs
+++ Assets/Scripts/Map/Spinning.cs
@@ -4,9 +4,15 @@ using UnityEngine;
 
 public class Spinning : MonoBehaviour
 {
+    private bool test;
 
     private void Update() {
+        if (test)
         gameObject.transform.Rotate(Vector3.up * 1);
+    }
+
+    public void Off(bool a) {
+        test = a;
     }
 
 }
