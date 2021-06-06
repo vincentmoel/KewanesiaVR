4796ad37e0738bea0361a35143ea2671e42d00cb
Aditya Putra <adtptra@gmail.com>
2021-06-06 18:19:36 +0700

    Bug fixing minigame1
    Fixing bug minigame 1, monyet tidak tampil lagi saat merestart minigame setelah menyelesaikan minigame
    Membuat minigame 1 memiliki bgm yang berbeda

--- Assets/Scripts/Minigame/MinigameMonyet.cs
+++ Assets/Scripts/Minigame/MinigameMonyet.cs
@@ -8,10 +8,14 @@ public class MinigameMonyet : MonoBehaviour
     private int count;
     public GameObject monyet;
     public GameObject animalCounter;
+
     public AudioClip finishSound;
+    public AudioClip bgmSound;
+    public AudioClip gameSound;
+    public AudioSource bgmSource;
     private AudioSource finishSoundSource;
+
     public List<GameObject> listMonyet;
-    
 
     public TextMeshProUGUI textCounter;
 
@@ -24,7 +28,55 @@ public class MinigameMonyet : MonoBehaviour
     {
         finishSoundSource = GetComponent<AudioSource>();
         finishSoundSource.clip = finishSound;
+    }
 
+
+    private void OnTriggerEnter(Collider other)
+    {
+
+        if (other.gameObject.CompareTag("Monyet"))
+        {
+            other.gameObject.SetActive(false);
+            count++;
+            textCounter.text = "Monyet : " + count.ToString() + "/5";
+
+            if (count == 5)
+            {
+                textCounter.text = "Misi selesai !";
+                textCounter.fontSize = 100;
+                textCounter.alignment = TextAlignmentOptions.Center;
+
+                finishSoundSource.Play();
+                StartCoroutine(CounterTimer());
+            }
+        }
+    }
+
+    private IEnumerator CounterTimer()
+    {
+        yield return new WaitForSeconds(5);
+        monyet.SetActive(false);
+        animalCounter.SetActive(false);
+        dataHewanMonyet.OpenAnimal();
+        finishSoundSource.Stop();
+        bgmSource.clip = bgmSound;
+        bgmSource.Play();
+        ObjPlaceAnimal.SetActive(transform);
+        ResetMiniGame();
+    }
+
+    public void ResetMiniGame()
+    {
+
+        //reset the text canvas
+        count = 0;
+        textCounter.text = "Monyet : " + count.ToString() + "/5";
+        textCounter.alignment = TextAlignmentOptions.TopLeft;
+        textCounter.fontSize = 32;
+    }
+
+    public void ResetMonkeysPosition()
+    {
         List<int> monyetRandom = new List<int>();
         while (monyetRandom.Count != 5)
         {
@@ -39,37 +91,9 @@ public class MinigameMonyet : MonoBehaviour
         {
             listMonyet[index].SetActive(true);
         }
-    }
 
-    
-    private void OnTriggerEnter(Collider other) {
-
-        if (other.gameObject.CompareTag("Monyet")) {
-            Destroy(other.gameObject);
-            count++;
-            textCounter.text = "Monyet : " + count.ToString() + "/5";
-            
-            if (count == 1)
-            {
-                textCounter.text = "Misi selesai !";
-                textCounter.fontSize = 100;
-                textCounter.alignment = TextAlignmentOptions.Center;
-                
-                finishSoundSource.Play();
-                StartCoroutine(CounterTimer());
-            }
-        }
+        //play the audio game
+        bgmSource.clip = gameSound;
+        bgmSource.Play();
     }
-
-    private IEnumerator CounterTimer()
-    {
-        yield return new WaitForSeconds(5);
-        monyet.SetActive(false);
-        animalCounter.SetActive(false);
-        dataHewanMonyet.OpenAnimal();
-        finishSoundSource.Stop();
-        ObjPlaceAnimal.SetActive(transform);
-    }
-    
-    
 }
