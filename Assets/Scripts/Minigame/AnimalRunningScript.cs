97d39210aa44327ba55ba156382e2b8dcb5bd9d3
sstevenadi <sstevenadi@gmail.com>
2021-06-15 18:59:48 +0700

    Up nambah asset
    Asset animal

---
+++ Assets/Scripts/Minigame/AnimalRunningScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class AnimalRunningScript : MonoBehaviour
+{
+    [Tooltip("Check, apakah player dekat atau tidak")]
+    public bool isPlayerNear = false;
+    public NavMeshAgent agent; //Variabel agent
+
+    public Transform playerPos;
+
+
+    public void Update()
+    {
+        if (isPlayerNear)
+        {
+            float distance = Vector3.Distance(playerPos.position, transform.position); //To get player position and this object position
+
+            //make AI run away
+            Vector3 dirToPlayer = transform.position - playerPos.position; //To get a new position for target
+            Vector3 newpos = transform.position + dirToPlayer; //To make a new position based on dirToPlayer distance
+            agent.SetDestination(newpos); //Move the object
+        }
+    }
+
+
+    private void OnTriggerEnter(Collider other) //To check if the player is near or not
+    {
+        if(other.CompareTag("Player"))
+        {
+            isPlayerNear = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other) //To check if the player is near or not
+    {
+        if (other.CompareTag("Player"))
+        {
+            isPlayerNear = false;
+        }
+    }
+
+
+}
