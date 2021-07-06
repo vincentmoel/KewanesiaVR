9e9f50f756de86bbf37ec19dd5d92a74f5094abd
Aditya Putra <adtptra@gmail.com>
2021-07-06 13:58:44 +0700

    Add animation papan ( enter, idle, exit )
    - Nambahin animasi papan untuk enter, idle, dan exit
    - Nambah script buat ngecek collider, player sudah masuk area hewan tersebut atau belum, jika masuk, akan nge run anim papan enter, jika keluar, akan nge run anim exit papan

---
+++ Assets/AnimalColliderToPlayer.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.Events;
+using UnityEngine.EventSystems;
+
+public class AnimalColliderToPlayer : MonoBehaviour
+{
+    [System.Serializable]
+    public class PapanSetting
+    {
+
+        [Header("Event untuk reset papan ketika papan masuk")]
+        public UnityEvent papanEvent;
+        
+
+        public void ResetTampilanPapan()
+        {
+            papanEvent.Invoke();
+        }
+    }
+    public PapanSetting papan;
+
+
+
+    public Animator anim_papan;
+    public bool isPlayerNear;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            papan.ResetTampilanPapan();
+            isPlayerNear = true;
+            anim_papan.ResetTrigger("exit");
+            anim_papan.SetTrigger("enter");
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            papan.ResetTampilanPapan();
+            isPlayerNear = false;
+            anim_papan.ResetTrigger("enter");
+            anim_papan.SetTrigger("exit");
+        }
+    }
+}
---
+++ Assets/OnenableDisableObj.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Events;
+
+public class OnenableDisableObj : MonoBehaviour
+{
+    public UnityEvent onEnableFunc;
+    public UnityEvent onDisableFunc;
+
+    private void OnEnable()
+    {
+        onEnableFunc.Invoke();
+    }
+
+    private void OnDisable()
+    {
+        onDisableFunc.Invoke();
+    }
+}
