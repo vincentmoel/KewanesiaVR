59639f46f2f0b7cf94442fc3e4d73c58f28c9dec
Aditya Putra <adtptra@gmail.com>
2021-07-23 16:32:32 +0700

    adding button effect
    - button effect main menu
    - button effect papan hutan
    - sfx confetty

--- Assets/Scripts/AnimalColliderToPlayer.cs
+++ Assets/Scripts/AnimalColliderToPlayer.cs
@@ -50,6 +50,7 @@ public class AnimalColliderToPlayer : MonoBehaviour
     public GameObject btn_maingames;
     private void Awake()
     {
+        PlayerPrefs.DeleteAll();
         if (animal_data.getStatusHewan())
         {
             TurnOnAllButton();
