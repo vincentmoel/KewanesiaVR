d51c4a6c88d587ffbeb7bd8506da3cace0ede12e
Aditya Putra <adtptra@gmail.com>
2021-07-23 13:45:26 +0700

    Fixing hiu

--- Assets/Scripts/MiniGame3Movement.cs
+++ Assets/Scripts/MiniGame3Movement.cs
@@ -12,30 +12,27 @@ public class MiniGame3Movement : MonoBehaviour
     void Start()
     {
         sharkAnim.SetBool("isSwimming", true);
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        Quaternion tes = transform.localRotation;
+
+
+
+
         
-
-        //transform.Translate(forward * speed * Time.deltaTime);
-
-
-        //Vector3 vector3 = new Vector3();
-        //vector3.x = vrCamera.localRotation.x;
-        //vector3.y = vrCamera.localRotation.y;
-        //vector3.z = vrCamera.localScale.z;
-        //transform.Rotate(vector3);
-
-        transform.localRotation = Quaternion.Euler(vrCamera.localRotation.eulerAngles.x, vrCamera.localRotation.eulerAngles.y, vrCamera.localRotation.eulerAngles.z);
-
-        vrCamera.localRotation = Quaternion.Euler(Vector3.zero);
-
-
-        //Vector3 forward = transform.TransformDirection(Vector3.forward);
-        //rb.velocity = forward * speed;
         transform.Translate(Vector3.forward * speed * Time.deltaTime);
     }
+
+    private void LateUpdate()
+    {
+        transform.localRotation = vrCamera.localRotation;
+        vrCamera.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+
 }
