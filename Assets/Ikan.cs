9cbd2b185b946483a80ff4b62a050e873771c48e
sstevenadi <sstevenadi@gmail.com>
2021-07-23 15:46:15 +0700

    up minigame 3
    setelah ini lanjut bikin biar tombol bisa start minigame

--- Assets/Ikan.cs
+++ Assets/Ikan.cs
@@ -2,22 +2,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using Random = System.Random;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
 
 public class Ikan : MonoBehaviour
 {
     public GameObject ikans;
+    public GameObject bombs;
     public Vector3 center;
     public Vector3 size;
-    
-    
-    private float nextActionTime = 0.0f;
+    public bool state;
+
+    [SerializeField] public List<GameObject> totIkan;
+    [SerializeField] public List<GameObject> totBomb;
+    public float nextActionTime = 0.0f;
     public float period = 1f;
  
     void Update () {
+        if (!state)
+        {
+            return;
+        }
         if (Time.time > nextActionTime ) { //
             nextActionTime += period;
-            SpawnObjectAtRandom();
+            if (totIkan.Count < 35 || totBomb.Count < 5) SpawnObjectAtRandom();
         }
     }
 
@@ -27,7 +35,35 @@ public class Ikan : MonoBehaviour
             UnityEngine.Random.Range(-size.y / 2, size.y / 2), 
             UnityEngine.Random.Range(-size.z / 2, size.z / 2));
 
-        Instantiate(ikans, randomPos, Quaternion.identity);
+        GameObject objeckSpawn = new GameObject();
+        switch (Random.Range(0,2))
+        {
+            case 0 :
+                if (totBomb.Count < 5)
+                {
+                    objeckSpawn = Instantiate(bombs, randomPos, Quaternion.identity);
+                    objeckSpawn.tag = "Bomb";
+                    objeckSpawn.SetActive(true);
+                    totBomb.Add(objeckSpawn);
+                }
+                else
+                {
+                    objeckSpawn = Instantiate(ikans, randomPos, Quaternion.identity);
+                    objeckSpawn.tag = "Ikan";
+                    objeckSpawn.SetActive(true);
+                    totIkan.Add(objeckSpawn);
+                }
+
+                break;
+            
+            case 1 :
+                objeckSpawn= Instantiate(ikans, randomPos, Quaternion.identity);
+                objeckSpawn.tag = "Ikan";
+                objeckSpawn.SetActive(true);
+                totIkan.Add(objeckSpawn);
+                
+                break;
+        }
     }
 
     private void OnDrawGizmosSelected()
--- Assets/Scripts/MiniGame3Movement.cs
+++ Assets/Scripts/MiniGame3Movement.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 
 public class MiniGame3Movement : MonoBehaviour
@@ -7,32 +9,70 @@ public class MiniGame3Movement : MonoBehaviour
     public Transform vrCamera;
     public float speed = 10.0f;
     public Rigidbody rb;
-
+    public Ikan ikan;
     public Animator sharkAnim;
+    public AudioSource feedSource;
+    public AudioClip feedSound;
+    public AudioClip bombSound;
+    private int score = 0;
+    public TextMeshProUGUI textCounter;
+    public GameObject tutor;
+    public GameObject animal;
+
     void Start()
     {
         sharkAnim.SetBool("isSwimming", true);
-
+        feedSource.clip = feedSound;
     }
 
     // Update is called once per frame
     void Update()
     {
-
-
-        Quaternion tes = transform.localRotation;
-
-
-
-
+        if (!ikan.state)
+        {
+            return;
+        } 
         
-        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+        Quaternion tes = transform.localRotation;
+        
+        transform.Translate(Vector3.forward * (speed * Time.deltaTime));
     }
 
     private void LateUpdate()
     {
         transform.localRotation = vrCamera.localRotation;
         vrCamera.localRotation = Quaternion.Euler(0, 0, 0);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Ikan"))
+        {
+            ikan.totIkan.Remove(other.gameObject);
+            feedSource.clip = feedSound;
+            feedSource.Play();
+            Destroy(other.gameObject);
+            score += 100;
+            textCounter.text = "Score : " + score.ToString();
+        }
+        
+        if (other.CompareTag("Bomb"))
+        {
+            ikan.totBomb.Remove(other.gameObject);
+            feedSource.clip = bombSound;
+            feedSource.Play();
+            Destroy(other.gameObject);
+            score -= 100;
+            textCounter.text = "Score : " + score.ToString();
+        }
+    }
+
+    public void StartGame()
+    {
+        ikan.state = true;
+        animal.SetActive(false);
+        textCounter.gameObject.SetActive(true);
+        tutor.SetActive(false);
     }
 
 }
