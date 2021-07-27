3492cbdf31cb28e39d1d792f7b3604b44310d740
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-07-27 16:23:38 +0700

    Rombak Ulang Main Menu
    Completed
    Tambahan : Fitur delete data

---
+++ Assets/Scripts/DeleteGameData.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteGameData : MonoBehaviour
+{
+    public void DeleteProgressData()
+    {
+        PlayerPrefs.DeleteAll();
+    }
+
+
+}
