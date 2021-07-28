70f4cf793895bc3c68838ffefb7ea888204f4dce
sstevenadi <sstevenadi@gmail.com>
2021-07-28 19:24:01 +0700

    Loading Script

---
+++ Assets/Scripts/Loading.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using TMPro;
+using UnityEngine;
+using UnityEngine.SocialPlatforms;
+using Random = UnityEngine.Random;
+
+public class Loading : MonoBehaviour
+{
+    private int loading;
+    public TextMeshProUGUI textCounter;
+    void Start()
+    {
+        StartCoroutine(RandomLoading());
+    }
+
+    private IEnumerator RandomLoading()
+    {
+        yield return new WaitForSeconds(Random.Range(0, 3));
+        
+        loading += Random.Range(10, 20);
+        
+        textCounter.text = loading + "%";
+        
+        if (loading >= 100)
+        {
+            textCounter.text = "100 %";
+            StopCoroutine(RandomLoading());
+        }
+        else
+        {
+            StartCoroutine(RandomLoading());
+        }
+        
+        
+    }
+}
