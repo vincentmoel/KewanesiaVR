2c27da49f478d9a5e188e60bbafb9419eefd75e4
Vincent Nathaniel M <vincentmoeljopranoto@gmail.com>
2021-08-03 14:06:33 +0700

    Kasi Panah tutor

---
+++ Assets/PanahLookAt.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PanahLookAt : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // transform.localRotation = Quaternion.Euler(90, transform.localRotation.eulerAngles.y + 180, 0);
+
+    }
+    
+    
+}
